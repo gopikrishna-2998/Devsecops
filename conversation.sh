ce820f34784a5e4c29307fc34f01c22bb118a65a
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-18 17:50:50 +0530

    convo bw friends

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=gopi
+person2=chanti
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hello $person1"
+echo "$person1:: all ok now $person2"
+echo "$person2:: im good now $person1"
