837786a0a84e1377dc635a8c70b609e16f1f9cc5
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-21 10:18:39 +0530

    variable calling with arugments

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "person1:: Are you playing cricket"
+echo "person2:: Yes Im playing"
+echo "person1:: where youre playing"
+echo "person2:: In my home town"
