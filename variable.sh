686ef4528d6bb600b3d70015781cb8c125fc69a3
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-21 10:24:12 +0530

    calling with arguments

--- variable.sh
+++ variable.sh
@@ -5,5 +5,5 @@ person2=$2
 
 echo "$person1:: Are you playing cricket"
 echo "$person2:: Yes Im playing"
-echo "$person1:: where youre playing"
+echo "$person1:: where you playing now"
 echo "$person2:: In my home town"
