96f80e67c59af87750e95afb32a745d202740168
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-21 11:00:51 +0530

    exevariable

--- excute-command.sh
+++ excute-command.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-Date=$(date)
-echo "date: $Date"
+#Date=$(date)
+
+Start_time=$(date +%s)
+
+End_time=$(date +%s)
+
+total_time=$(($End_time-$Start_time))
+echo "date: $total_time in seconds"
