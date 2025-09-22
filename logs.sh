1448d1ee9565cada96b80ef47ceb022952eb7ea2
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-22 19:22:13 +0530

    logs

---
+++ logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+LOGS_FOLDER="var/log/Devsecops"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+
+mkdir -p $LOGS_FOLDER
+echo "script exicuted $(date)"
+
+if [ $USERID -ne 0 ]; then
+   echo "error: check the root privelages"
+   exit 1
+fi
+validate(){
+    if [ $1 -ne 0 ]; then
+       echo "error $2 installing failure"
+       exit 1
+    else
+        echo "installing $2 is success"
+    fi
+}
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+   dnf install nginx -y
+   validate $? "nginx"
+else
+    echo -e "nginx already $g installed $n"
+fi
