bb99b34390e846b91a112fe4408a9ac9c634159d
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-22 18:13:57 +0530

    installation

--- instllation.sh
+++ instllation.sh
@@ -2,14 +2,14 @@
 
 USERID=$(id -u)
 
-if [ $USERID -eq 0 ]; then 
+if [ $USERID -ne 0 ]; then 
    echo "error: check the root access"
    exit 1
 fi
 
 dnf install redis -y
 
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
    echo "error: check the process code"
    exit 1
 
