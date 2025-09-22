52902d6fed6490ce711c8b0db391e6c4c0c74100
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-22 11:17:36 +0530

    specialvariable

--- special_variables.sh
+++ special_variables.sh
@@ -3,3 +3,6 @@
 echo "All variabales passed by script:: $@"
 echo "All variabales passed by script:: $*"
 echo "script name: $0"   #$0 is used to get scriptname 
+echo "directory of this scrip: $PWD"
+echo "who is running: $USER"
+echo "home directory: $HOME"
