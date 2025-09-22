b527c03969e880525986a8692baab6e7de5f72bd
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-22 17:36:31 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -3,8 +3,8 @@
 #conditions
 number=$1
 
-if [$number -lt 10]; then
+if [$number  -lt 10 ]; then
     echo "given number $number is less than 10"
 else
-    echo "given number $number is grether or equal 10"
+    echo "given number $number is greater than or equal 10"
 fi
