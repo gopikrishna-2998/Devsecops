208c5f917f11218bb853434903546c2651ac5bd5
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-22 11:40:02 +0530

    data_types

--- data_types.sh
+++ data_types.sh
@@ -2,7 +2,11 @@
 # every think under shell is consider as string
 number1=100
 number2=200
+name=Gopi
 
 sum=$(($number1 + $number2))
 
 echo "sum of two numbers: $sum"
+
+FRIENDS=("Dhoni" "KOHLI" "Rohit")
+echo "all friends:: $FRIENDS[*]"
